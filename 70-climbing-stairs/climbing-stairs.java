dda0326f1b3539f6bda63922535b9b3b5b6fbab9
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-01 14:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (71.90%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] res = new int[n + 2];
+        res[0] = 0;
+        res[1] = 1;
+        res[2] = 2;
+        
+        for (int i = 3; i <= n; i++)
+            res[i] = res[i - 1] + res[i - 2];
+        
+        return res[n];
+    }
+}

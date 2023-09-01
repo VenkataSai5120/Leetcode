460a136171173fd4cc1c48d4d073f4574cc3717c
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-01 15:14:40 +0530

    Time: 495 ms (50.21%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
+# SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+# FROM Activity a1, Activity a2
+# WHERE a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id AND a2.timestamp > a1.timestamp
+# GROUP BY a1.machine_id;
+
 SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
-FROM Activity a1, Activity a2
+FROM Activity a1
+INNER JOIN
+Activity a2
 WHERE a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id AND a2.timestamp > a1.timestamp
 GROUP BY a1.machine_id;

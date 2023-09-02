97af1173dadab5f65f42ce378f885088de985679
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-02 22:39:01 +0530

    Time: 1307 ms (30.68%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -2,7 +2,7 @@
 SELECT 
     DATE_FORMAT(trans_date, "%Y-%m") AS month, 
     country, 
-    COUNT(trans_date) AS trans_count, 
+    COUNT(*) AS trans_count, 
     SUM(IF (state = 1, 1, 0)) AS approved_count, 
     SUM(amount) AS trans_total_amount, 
     SUM(IF (state = 1, amount, 0)) AS approved_total_amount

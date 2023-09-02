9d6fdc41f2cb2bfc3f6356ee023f034688802d9e
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-02 22:40:52 +0530

    Time: 1016 ms (81.81%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -3,8 +3,8 @@ SELECT
     DATE_FORMAT(trans_date, "%Y-%m") AS month, 
     country, 
     COUNT(*) AS trans_count, 
-    SUM(IF (state = 1, 1, 0)) AS approved_count, 
+    SUM(CASE WHEN state = 1 THEN 1 ELSE 0 END) AS approved_count, 
     SUM(amount) AS trans_total_amount, 
-    SUM(IF (state = 1, amount, 0)) AS approved_total_amount
+    SUM(CASE WHEN state = 1 THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY DATE_FORMAT(trans_date, "%Y-%m"), country;

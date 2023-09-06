153f87055a01c51afd63430d5654f65cbee628d3
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-06 21:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (90.12%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.java
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -17,19 +17,21 @@ class Solution {
             size++;
         }
 
-        int partLength = size / k, extra = size % k;
+        int partLen = size / k, extra = size % k;
 
         ListNode[] ans = new ListNode[k];
         curr = root;
-        for (int i = 0; i < k; i++) {
-            ListNode head = new ListNode(0), dup = head;
-
-            for (int j = 0; j < partLength + (i < extra ? 1 : 0); j++) {
-                dup = dup.next = new ListNode(curr.val);
-                curr = curr.next;
+        for (int i = 0; i < k; ++i) {
+            ListNode head = curr;
+            for (int j = 0; j < partLen + (i < extra ? 1 : 0) - 1; ++j) {
+                if (curr != null) curr = curr.next;
             }
-            
-            ans[i] = head.next;
+            if (curr != null) {
+                ListNode prev = curr;
+                curr = curr.next;
+                prev.next = null;
+            }
+            ans[i] = head;
         }
         return ans;
     }

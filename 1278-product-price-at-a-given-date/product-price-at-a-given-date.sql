db5d1e3d4ee98c958646bd2583b3528e8f766972
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-09 20:14:06 +0530

    Time: 1220 ms (29.98%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -3,7 +3,7 @@ SELECT product_id, 10 AS price
 FROM Products
 GROUP BY product_id
 HAVING MIN(change_date) > '2019-08-16'
-UNION
+UNION ALL
 SELECT product_id, new_price AS price
 FROM Products
 WHERE (product_id, change_date) IN (

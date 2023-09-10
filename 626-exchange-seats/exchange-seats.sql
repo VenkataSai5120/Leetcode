d6a554a943a430cd20c359b34acce8c6a262043a
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-10 18:44:48 +0530

    Time: 955 ms (28.20%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-SELECT (CASE
-        WHEN MOD(id, 2) = 1 AND total_seats = id THEN id
-        WHEN MOD(id, 2) = 1 AND total_seats <> id THEN id + 1
-        ELSE id - 1
-    END) AS id, student
-FROM Seat, (SELECT COUNT(*) AS total_seats FROM Seat) AS count_seats
+# SELECT (CASE
+#         WHEN MOD(id, 2) = 1 AND total_seats = id THEN id
+#         WHEN MOD(id, 2) = 1 AND total_seats <> id THEN id + 1
+#         ELSE id - 1
+#     END) AS id, student
+# FROM Seat, (SELECT COUNT(*) AS total_seats FROM Seat) AS count_seats
+# ORDER BY id;
+
+SELECT IF(MOD(id, 2) = 1 AND total_seats = id, id, (id + 1) ^ 1 - 1) AS id, student
+FROM seat, (SELECT COUNT(*) AS total_seats FROM Seat) AS count_seats
 ORDER BY id;

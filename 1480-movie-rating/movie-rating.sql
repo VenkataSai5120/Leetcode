1801040b35286ef0d42f30edea1f35a3c25352e2
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-10 19:11:28 +0530

    Time: 3442 ms (23.95%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 (SELECT u.name AS results
 FROM MovieRating AS mr
-JOIN Users AS u ON mr.user_id = u.user_id
+INNER JOIN Users AS u ON mr.user_id = u.user_id
 GROUP BY mr.user_id
 ORDER BY COUNT(mr.user_id) DESC, u.name
 LIMIT 1)
 UNION ALL
 (SELECT m.title
 FROM MovieRating AS mr
-JOIN Movies AS m ON mr.movie_id = m.movie_id
+INNER JOIN Movies AS m ON mr.movie_id = m.movie_id
 WHERE DATE_FORMAT(mr.created_at, "%m-%Y") = "02-2020"
 GROUP BY mr.movie_id
 ORDER BY AVG(rating) DESC, m.title

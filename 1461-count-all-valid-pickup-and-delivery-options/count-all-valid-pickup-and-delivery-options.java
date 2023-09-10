fc6a4a4514695f1390f40e9bedd701a95d794c2c
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-09-10 22:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (48.78%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -3,12 +3,14 @@
 class Solution {
     private static final int mod = 1_000_000_007;
     public int countOrders(int n) {
-        // int[] dp = 
-        return findWays(n);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return findWays(n, dp);
     }
 
-    private int findWays(int n) {
+    private int findWays(int n, int[] dp) {
         if (n == 0) return 1;
-        return (int)((2 * (long)n - 1) * n * findWays(n - 1) % mod);
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = (int)((2 * (long)n - 1) * n * findWays(n - 1, dp) % mod);
     }
 }

de7e99347a3d9e57e0b8785b283f59ef3b3721f5
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-03 18:18:36 +0530

    Time: 1 ms (48.39%) | Memory: 40.1 MB (17.74%) - LeetSync

---
+++ 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        String num = Integer.toString(n);
+        int[][][] dp = new int[10][2][10];
+        for (int[][] a : dp) {
+            for (int[] b : a) Arrays.fill(b, -1);
+        }
+        return findNums(num.toCharArray(), 0, 0, 0, digits, dp) - 1;
+    }
+
+    private int findNums(char[] num, int index, int decider, int prevDig, 
+    String[] digits, int[][][] dp) {
+        if (index == num.length) return 1;
+        if (dp[index][decider][prevDig] != -1) return dp[index][decider][prevDig];
+
+        int res = 0;
+        int limit = decider == 1 ? digits[digits.length - 1].charAt(0) - '0' : num[index] - '0';
+
+        if (prevDig == 0) res += findNums(num, index + 1, 1, prevDig, digits, dp);
+
+        for (String dig : digits) {
+            int d = Integer.parseInt(dig);
+            if (d > limit) break;
+            int newDecider = d < limit ? 1 : decider;
+            res += findNums(num, index + 1, newDecider, d, digits, dp);
+        }
+
+        return dp[index][decider][prevDig] = res;
+    }
+}

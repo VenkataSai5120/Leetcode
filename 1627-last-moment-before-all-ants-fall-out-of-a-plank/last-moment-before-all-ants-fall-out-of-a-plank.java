185fe2ff83551cec8c1a0aadf32dbe40469ab89e
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-04 14:56:06 +0530

    Time: 6 ms (16.48%) | Memory: 43.4 MB (94.51%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -2,17 +2,16 @@ class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
         Arrays.sort(left);
         Arrays.sort(right);
-        reverse(right);
-        int i = 0, j = 0;
+        int i = 0, j = right.length - 1;
         int maxTime = 0;
 
-        while (i < left.length && j < right.length) {
-            double midPoint = left[j] - (left[i] + right[j]) / 2.0;
+        while (i < left.length && j >= 0) {
+            double midPoint = left[i] - (left[i] + right[j]) / 2.0;
             // System.out.println(midPoint);
             int leftDistance = (int)(2 * midPoint) + n - left[i];
             int rightDistance = (int)(2 * midPoint) + right[j];
             maxTime = Math.max(maxTime, Math.max(leftDistance, rightDistance));
-            i++; j++;
+            i++; j--;
         }
 
         while (i < left.length) {
@@ -20,21 +19,11 @@ class Solution {
             i++;
         }
 
-        while (j < right.length) {
+        while (j >= 0) {
             maxTime = Math.max(maxTime, n - right[j]);
-            j++;
+            j--;
         }
 
         return maxTime;
-    }
-
-    private void reverse(int[] array) {
-        int n = array.length;
-
-        for (int i = 0; i < n / 2; i++) {
-            int temp = array[i];
-            array[i] = array[n - i - 1];
-            array[n - i - 1] = temp;
-        }
     }
 }

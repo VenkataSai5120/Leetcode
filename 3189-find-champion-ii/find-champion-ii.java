2a74bebd813ec8866fff957d59b0c9ba1da9568d
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-05 16:18:20 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (33.33%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+        int[] countSuperiors = new int[n];
+        
+        for (int[] edge : edges) {
+            countSuperiors[edge[1]]++;
+        }
+
+        int winner = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (countSuperiors[i] == 0) {
+                if (winner != -1) return -1;
+                winner = i;
+            }
+        }
+
+        return winner;
+    }
+}

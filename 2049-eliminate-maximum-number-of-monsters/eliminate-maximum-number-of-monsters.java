f1307ce46266fa96cee00b8100ac573957080952
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-07 21:41:48 +0530

    Time: 16 ms (100.00%) | Memory: 53.9 MB (98.96%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
@@ -1,20 +1,11 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
-        int n = speed.length;
-        PriorityQueue<Double> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < n; i++) {
-            pq.offer(dist[i] * 1.0 / speed[i]);
-        }
-
-        int time = 0;
-
-        while (!pq.isEmpty()) {
-            double monster = pq.poll();
-            if (monster <= time) return n - pq.size() - 1;
-            time++;
-        }
-
-        return n;
-    }
+    for (int i = 0; i < dist.length; ++i)
+        dist[i] = (dist[i] - 1) / speed[i];
+    Arrays.sort(dist);
+    for (int i = 0; i < dist.length; ++i)
+        if (i > dist[i])
+            return i;
+    return dist.length;
+}
 }

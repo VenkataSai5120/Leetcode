789124e3b286c82ccdc1b630ec44b3c027ef5898
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-09 00:40:11 +0530

    Time: 9 ms (25.25%) | Memory: 42.4 MB (25.25%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
@@ -1,11 +1,16 @@
 class Solution {
     public int numDupDigitsAtMostN(int n) {
         String num  = Integer.toString(n);
-        return n - findNoDup(num, num.length(), 0, 0, 0);
+        int[][][] dp = new int[num.length()][2][1 << 10];
+        for (int[][] a : dp) {
+            for (int[] b : a) Arrays.fill(b, -1);
+        }
+        return n - findNoDup(num, num.length(), 0, 0, 0, dp);
     }
 
-    private int findNoDup(String num, int n, int index, int decider, int mask) {
+    private int findNoDup(String num, int n, int index, int decider, int mask, int[][][] dp) {
         if (index == n) return mask == 0 ? 0 : 1;
+        if (dp[index][decider][mask] != -1) return dp[index][decider][mask];
         int res = 0;
         int limit = decider == 1 ? 9 : num.charAt(index) - '0';
 
@@ -13,9 +18,9 @@ class Solution {
             if ((mask & (1 << i)) != 0) continue;
             int newDecider = i < limit ? 1 : decider;
             int newMask = i == 0 && mask == 0 ? mask : mask | (1 << i);
-            res += findNoDup(num, n, index + 1, newDecider, newMask);
+            res += findNoDup(num, n, index + 1, newDecider, newMask, dp);
         }
 
-        return res;
+        return dp[index][decider][mask] = res;
     }
 }

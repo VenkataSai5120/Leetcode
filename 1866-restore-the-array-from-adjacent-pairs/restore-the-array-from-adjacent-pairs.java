b577ccb7e22cd02186288a905fb9c32fb735fb61
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-10 23:45:35 +0530

    Time: 103 ms (70.08%) | Memory: 108.7 MB (34.02%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
@@ -1,43 +1,31 @@
 class Solution {
     public int[] restoreArray(int[][] adjacentPairs) {
-        Map<Integer, Integer> indegree = new HashMap<>();
         Map<Integer, List<Integer>> adj = new HashMap<>();
         int start = adjacentPairs[0][0];
 
         for (int[] pair : adjacentPairs) {
-            int id1 = indegree.getOrDefault(pair[0], 0) + 1;
-            int id2 = indegree.getOrDefault(pair[1], 0) + 1;
-            indegree.put(pair[0], id1);
-            indegree.put(pair[1], id2);
             adj.computeIfAbsent(pair[0], val -> new ArrayList<>()).add(pair[1]);
             adj.computeIfAbsent(pair[1], val -> new ArrayList<>()).add(pair[0]);
         }
 
-        for (Map.Entry<Integer, Integer> map : indegree.entrySet()) {
-            if (map.getValue() == 1) {
+        for (Map.Entry<Integer, List<Integer>> map : adj.entrySet()) {
+            if (map.getValue().size() == 1) {
                 start = map.getKey();
                 break;
             }
         }
 
-        List<Integer> res = new ArrayList<>();
-        Set<Integer> vis = new HashSet<>();
-        dfs(adj, start, vis, res);
-        int[] result = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            result[i] = res.get(i);
-        }
+        int[] result = new int[adj.size()];
+        dfs(adj, start, 1_000_000_007,result, 0);
         return result;
     }
 
-    private void dfs(Map<Integer, List<Integer>> adj, int src, 
-    Set<Integer> vis, List<Integer> res) {
-        vis.add(src);
-        res.add(src);
+    private void dfs(Map<Integer, List<Integer>> adj, int src, int par, int[] res, int index) {
+        res[index] = src;
 
         for (int next : adj.getOrDefault(src, new ArrayList<>())) {
-            if (!vis.contains(next)) {
-                dfs(adj, next, vis, res);
+            if (next != par) {
+                dfs(adj, next, src, res, index + 1);
             }
         }
     }

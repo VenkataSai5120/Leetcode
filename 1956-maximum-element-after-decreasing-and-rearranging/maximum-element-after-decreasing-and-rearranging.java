6cb11c232f8abb01ebcce79d3dc82fdc0195639a
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-15 18:21:50 +0530

    Time: 6 ms (82.14%) | Memory: 56.6 MB (28.57%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -1,18 +1,23 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        // Arrays.sort(arr);
+        // arr[0] = 1;
+        // int maxEle = 1;
+
+        // for (int i = 1; i < arr.length; i++) {
+        //     if (Math.abs(arr[i - 1] - arr[i]) <= 0) {
+        //     }
+        //     else {
+        //         arr[i] = arr[i - 1] + 1;
+        //     }
+        //     maxEle = Math.max(maxEle, arr[i]);
+        // }
+
+        // return maxEle;
         Arrays.sort(arr);
-        arr[0] = 1;
-        int maxEle = 1;
-
-        for (int i = 1; i < arr.length; i++) {
-            if (Math.abs(arr[i - 1] - arr[i]) <= 0) {
-            }
-            else {
-                arr[i] = arr[i - 1] + 1;
-            }
-            maxEle = Math.max(maxEle, arr[i]);
-        }
-
-        return maxEle;
+        int prev = 0;
+        for (int ele : arr)
+            prev = Math.min(prev + 1, ele);
+        return prev;
     }
 }

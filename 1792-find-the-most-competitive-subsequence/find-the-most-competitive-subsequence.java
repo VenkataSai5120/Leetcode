9340503ae4daf8b4c23a73c44f930aafe0d1ea7f
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-28 18:16:07 +0530

    Time: 63 ms (55.09%) | Memory: 59 MB (53.29%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -1,3 +1,35 @@
+// class Solution {
+//     public int[] mostCompetitive(int[] nums, int k) {
+//         int[] res = new int[k];
+//         int index = 0;
+//         Stack<Integer> stack = new Stack<>();
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+//             int rem = k - stack.size();
+
+//             if (nums[i] == 4) {
+//                 System.out.println(stack + " " + stack.size());
+//             }
+            
+//             if (n - i == rem) {
+//                 stack.push(nums[i]);
+//                 continue;
+//             }
+
+//             while (!stack.isEmpty() && stack.peek() > nums[i]) stack.pop();
+//             stack.push(nums[i]);
+//         }
+
+//         for (int ele : stack) {
+//             res[index++] = ele;
+//             if (index == k) break;
+//         }
+
+//         return res;
+//     }
+// }
+
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
         int[] res = new int[k];
@@ -7,17 +39,13 @@ class Solution {
         int remove = n - k;
 
         for (int i = 0; i < n; i++) {
-            // int rem = k - stack.size();
-            
-            // if (n - i == rem) {
-            //     stack.push(nums[i]);
-            //     continue;
-            // }
+            int rem = k - stack.size();
 
             while (!stack.isEmpty() && remove > 0 &&  stack.peek() > nums[i]) {
                 stack.pop();
                 remove--;
             }
+            
             stack.push(nums[i]);
         }
 

13de4261f11cc7d969399930e8b8e833353b2a8b
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-30 12:23:39 +0530

    Time: 3 ms (26.53%) | Memory: 40.1 MB (15.31%) - LeetSync

---
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[][][] dp;
+
+    public int f(int i, int prev, int constraint, String s) {
+        int n = s.length();
+        if (i >= n)
+            return 1;
+
+        if (dp[prev][constraint][i] != -1)
+            return dp[prev][constraint][i];
+
+        int answer = 0;
+        int ub = constraint == 1 ? s.charAt(i) - '0' : 1;
+
+        for (int x = 0; x <= ub; x++) {
+            if (prev == 1) {
+                if (x == 0)
+                    answer += f(i + 1, x, (constraint & ((x == ub) ? 1 : 0)), s);
+            } else {
+                answer += f(i + 1, x, (constraint & ((x == ub) ? 1 : 0)), s);
+            }
+        }
+        return dp[prev][constraint][i] = answer;
+    }
+
+    public int findIntegers(int n) {
+        dp = new int[2][2][33];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+        String s = sb.toString();
+        int sz = (int) (Math.log(n) / Math.log(2)) + 1;
+
+        while (sb.length() < 32) {
+            sb.insert(0, '0');
+        }
+        s = sb.substring(32 - sz);
+
+        int ans = f(0, 0, 1, s);
+        return ans;
+    }
+}

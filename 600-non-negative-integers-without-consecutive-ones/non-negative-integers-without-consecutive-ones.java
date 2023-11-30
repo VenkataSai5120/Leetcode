4b7fd84c6a453eb9c249af1dfae52661344dc1dd
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-11-30 12:42:59 +0530

    Time: 4 ms (24.49%) | Memory: 41.2 MB (7.14%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
@@ -1,46 +1,35 @@
 class Solution {
-    int[][][] dp;
+    public int findIntegers(int n) {
+        String num = Integer.toString(n, 2);
+        int[][][] dp = new int[32][2][2];
 
-    public int f(int i, int prev, int constraint, String s) {
-        int n = s.length();
-        if (i >= n)
-            return 1;
-
-        if (dp[prev][constraint][i] != -1)
-            return dp[prev][constraint][i];
-
-        int answer = 0;
-        int ub = constraint == 1 ? s.charAt(i) - '0' : 1;
-
-        for (int x = 0; x <= ub; x++) {
-            if (prev == 1) {
-                if (x == 0)
-                    answer += f(i + 1, x, (constraint & ((x == ub) ? 1 : 0)), s);
-            } else {
-                answer += f(i + 1, x, (constraint & ((x == ub) ? 1 : 0)), s);
-            }
+        for (int [][] a1 : dp) {
+            for (int[] a2 : a1) Arrays.fill(a2, -1);
         }
-        return dp[prev][constraint][i] = answer;
+
+        return findNonCons(num, 0, 0, 0, dp);
     }
 
-    public int findIntegers(int n) {
-        dp = new int[2][2][33];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+    private int findNonCons(String num, int index, int decider, int prev, int[][][] dp) {
+        if (index == num.length()) return 1;
+        if (dp[index][decider][prev] != -1) return dp[index][decider][prev];
+
+        int res = 0;
+        int limit = decider == 1 ? 1 : num.charAt(index) - '0';
+
+        for (int i = 0; i <= limit; i++) {
+            int newDecider = i < limit ? 1 : decider;
+            
+            if (prev == 1) {
+                if (i == 0) {
+                    res += findNonCons(num, index + 1, newDecider, i, dp);
+                }
+            }
+            else {
+                res += findNonCons(num, index + 1, newDecider, i, dp);
             }
         }
 
-        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
-        String s = sb.toString();
-        int size = (int) (Math.log(n) / Math.log(2)) + 1;
-
-        while (sb.length() < 32) {
-            sb.insert(0, '0');
-        }
-        s = sb.substring(32 - size);
-
-        int ans = f(0, 0, 1, s);
-        return ans;
+        return dp[index][decider][prev] = res;
     }
 }

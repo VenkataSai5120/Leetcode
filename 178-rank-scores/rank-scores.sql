f5f5bba11f44b5af954149da3186fd482602cff4
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-12-01 19:45:56 +0530

    Time: 565 ms (44.82%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
 
-SELECT s1.score, (SELECT COUNT(DISTINCT s2.score)
-                 FROM Scores AS s2
-                 WHERE s2.score >= s1.score) AS `rank`
-FROM Scores AS s1
-ORDER BY s1.score DESC;
+# SELECT s1.score, (SELECT COUNT(DISTINCT s2.score)
+#                  FROM Scores AS s2
+#                  WHERE s2.score >= s1.score) AS `rank`
+# FROM Scores AS s1
+# ORDER BY s1.score DESC;
+
+SELECT score, @rank := @rank + (@prev <> (@prev := score)) AS `rank`
+FROM Scores, 
+(SELECT @rank := 0, @prev := -1) AS new
+ORDER BY score DESC;

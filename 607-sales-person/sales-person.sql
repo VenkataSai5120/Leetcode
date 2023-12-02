75234b2e40d88a9de3c56973b523461c783d16df
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-12-02 12:35:31 +0530

    Time: 2818 ms (35.94%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-SELECT s.name FROM SalesPerson AS s 
-WHERE s.sales_id NOT IN (
-    SELECT o.sales_id 
-    FROM Orders AS o 
-    LEFT JOIN Company AS c 
-    ON c.com_id = o.com_id 
-    WHERE c.name = "RED"
+# SELECT s.name FROM SalesPerson AS s 
+# WHERE s.sales_id NOT IN (
+#     SELECT o.sales_id 
+#     FROM Orders AS o 
+#     LEFT JOIN Company AS c 
+#     ON c.com_id = o.com_id 
+#     WHERE c.name = "RED"
+# );
+
+ SELECT name FROM SalesPerson 
+WHERE sales_id NOT IN(
+    SELECT sales_id FROM Orders WHERE com_id IN (
+            SELECT com_id FROM Company WHERE name="RED" )
 );

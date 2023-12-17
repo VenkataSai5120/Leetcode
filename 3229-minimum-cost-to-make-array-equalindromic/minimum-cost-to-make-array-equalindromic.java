eab72fc1d7bfcd44b204b9df5b505717bbec3a53
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-12-17 12:34:04 +0530

    Time: 17 ms (100.00%) | Memory: 56.5 MB (66.67%) - LeetSync

---
+++ 3229-minimum-cost-to-make-array-equalindromic/minimum-cost-to-make-array-equalindromic.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long minimumCost(int[] nums) {
+        Arrays.sort(nums);
+        int med = 0, len = nums.length;
+        med = nums[len / 2];
+        int num1 = palindrome1(med), num2 = palindrome2(med);
+        long sum1 = findSum(nums, num1), sum2 = findSum(nums, num2);
+        return sum1 > sum2 ? sum2 : sum1;
+    }
+
+    private int palindrome1(int num) {
+        while (!check(num)) {
+            num--;
+        }
+        return num;
+    }
+
+    private int palindrome2(int num) {
+        while (!check(num)) {
+            num++;
+        }
+        return num;
+    }
+
+    private boolean check(int n) {
+        long dup = n, rev = 0;
+        while (dup != 0) {
+            rev = rev * 10 + (dup % 10);
+            dup /= 10;
+        }
+        return rev == n;
+    }
+
+    private long findSum(int nums[], int n) {
+        long sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + Math.abs(n - nums[i]);
+        }
+        return sum;
+    }
+}

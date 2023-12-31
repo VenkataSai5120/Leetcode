9093a28884273b941f4382c1cd3ac81fd19b8c0b
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2023-12-31 15:20:25 +0530

    Time: 332 ms (5.11%) | Memory: 100.8 MB (5.71%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,30 +1,35 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            res.add(new ArrayList<>());
-            adj.add(new ArrayList<>());
-        }
+        int[] inDegree = new int[n];
+        Map<Integer, List<Integer>> adj = new HashMap<>();
         
         for (int[] e : edges) {
-            adj.get(e[0]).add(e[1]);
+            adj.computeIfAbsent(e[0], l -> new ArrayList<>()).add(e[1]);
+            inDegree[e[1]]++;
         }
-        
-        for (int i = 0; i < n; i++) {
-            findAncestors(i, i, res, adj);
+
+        List<Set<Integer>> sets = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; ++i) {
+            sets.add(new HashSet<>());
+            if (inDegree[i] == 0)
+                q.offer(i);
         }
-        
-        return res;
-    }
-    
-    private void findAncestors(int x, int src, List<List<Integer>> res, List<List<Integer>> adj) {
-        for (int node : adj.get(src)) {
-            if (res.get(node).isEmpty() || res.get(node).get(res.get(node).size() - 1) != x) {
-                res.get(node).add(x);
-                findAncestors(x, node, res, adj);
+
+        while (!q.isEmpty()) {
+            int parent = q.poll();
+            for (int kid : adj.getOrDefault(parent, Arrays.asList())) {
+                sets.get(kid).add(parent);
+                sets.get(kid).addAll(sets.get(parent));
+                if (--inDegree[kid] == 0)
+                    q.offer(kid);
             }
         }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (Set<Integer> set : sets)
+            ans.add(new ArrayList<>(new TreeSet<>(set)));
+        return ans;
     }
 }

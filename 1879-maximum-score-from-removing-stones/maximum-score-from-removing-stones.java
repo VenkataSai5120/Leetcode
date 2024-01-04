3fd0b10c3c1d4cb2203d1f13880c79bd4d1f2a99
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-01-05 00:13:48 +0530

    Time: 30 ms (55.25%) | Memory: 62.9 MB (6.08%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
@@ -1,21 +1,25 @@
 class Solution {
     public int maximumScore(int a, int b, int c) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
-        pq.add(a);
-        pq.add(b);
-        pq.add(c);
-        int res = 0;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
+        // pq.add(a);
+        // pq.add(b);
+        // pq.add(c);
+        // int res = 0;
 
-        while (pq.size() >= 2) {
-            res++;
-            int top1 = pq.poll();
-            int top2 = pq.poll();
-            top1--;
-            top2--;
-            if (top1 > 0) pq.add(top1);
-            if (top2 > 0) pq.add(top2);
-        }
+        // while (pq.size() >= 2) {
+        //     res++;
+        //     int top1 = pq.poll();
+        //     int top2 = pq.poll();
+        //     top1--;
+        //     top2--;
+        //     if (top1 > 0) pq.add(top1);
+        //     if (top2 > 0) pq.add(top2);
+        // }
 
-        return res;
+        // return res;
+
+        if (a < b) return maximumScore(b, a, c);
+        if (b < c) return maximumScore(a, c, b);
+        return b == 0 ? 0 : 1 + maximumScore(a - 1, b - 1, c);
     }
 }

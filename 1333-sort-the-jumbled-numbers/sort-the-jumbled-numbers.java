342e307310c830cfb1c75118509c62f6e3a77145
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-01-05 12:36:26 +0530

    Time: 445 ms (24.04%) | Memory: 56.2 MB (12.50%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -27,11 +27,36 @@ class customSort implements Comparator<Integer> {
 }
 
 class Solution {
+    // public int[] sortJumbled(int[] mapping, int[] nums) {
+    //     customSort cs = new customSort(mapping);
+    //     Integer[] boxedNums = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+    //     Arrays.sort(boxedNums, cs);
+    //     int[] sortedNums = Arrays.stream(boxedNums).mapToInt(Integer::intValue).toArray();
+    //     return sortedNums;
+    // }
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        customSort cs = new customSort(mapping);
-        Integer[] boxedNums = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Arrays.sort(boxedNums, cs);
-        int[] sortedNums = Arrays.stream(boxedNums).mapToInt(Integer::intValue).toArray();
-        return sortedNums;
+        int n = nums.length, k = 0;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; ++i) indices[i] = i;
+        Arrays.sort(indices, Comparator.comparingInt(i -> convert(nums[i], mapping)));
+
+        int[] ans = new int[n];
+        for (int idx : indices)
+            ans[k++] = nums[idx];
+        return ans;
+    }
+
+    private int convert(int num, int[] mapping) {
+        if (num == 0)
+            return mapping[0];
+        int n = 0, f = 1;
+        
+        while (num > 0) {
+            n += mapping[num % 10] * f;
+            f *= 10;
+            num /= 10;
+        }
+
+        return n;
     }
 }

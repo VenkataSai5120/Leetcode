277cf9c69f5bd6033fec42a482e9b01f22ae0626
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-01-10 16:03:08 +0530

    Time: 5 ms (86.71%) | Memory: 44.9 MB (15.92%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        return find4Sum(nums, 0, 4, target);
+        return findQuads(nums, 0, 4, target);
     }
 
-    public List<List<Integer>> find4Sum(int[] nums, int start, int k, long target) {
+    private List<List<Integer>> findQuads(int[] nums, int index, int k, long tar) {
         List<List<Integer>> res = new ArrayList<>();
-        if (start == nums.length) return res;
+        if (index == nums.length) return res;
+        int n = nums.length;
+        long avgVal = tar / k;
+        if (nums[index] > avgVal || avgVal > nums[n - 1]) return res;
+        if (k == 2) return findPairs(nums, index, n - 1, tar);
 
-        long avgVal = target / k;
-        if (nums[start] > avgVal || avgVal > nums[nums.length - 1]) return res;
-        if (k == 2) return find2Sum(nums, start, target);
-
-        for (int i = start; i < nums.length; i++) {
-            if (i == start || nums[i] != nums[i - 1]) {
-                for (List<Integer> sub : find4Sum(nums, i + 1, k - 1, target - nums[i])) {
+        for (int i = index; i < n; i++) {
+            if (i == index || nums[i - 1] != nums[i]) {
+                for (List<Integer> sub : findQuads(nums, i + 1, k - 1, tar - nums[i])) {
                     res.add(new ArrayList<>(Arrays.asList(nums[i])));
                     res.get(res.size() - 1).addAll(sub);
                 }
@@ -24,19 +24,19 @@ class Solution {
         return res;
     }
 
-    public List<List<Integer>> find2Sum(int[] nums, int start, long target) {
+    private List<List<Integer>> findPairs(int[] nums, int low, int high, long tar) {
         List<List<Integer>> res = new ArrayList<>();
-
-        int low = start, high = nums.length - 1;
 
         while (low < high) {
             while (!res.isEmpty() && low < high && nums[low] == res.get(res.size() - 1).get(0)) low++;
-            while (!res.isEmpty() && low < high && nums[high] == res.get(res.size() - 1).get(1)) high--;         
-
+            while (!res.isEmpty() && low < high && nums[high] == res.get(res.size() - 1).get(1)) high--;
             if (low == high) return res;
-
-            if (nums[low] + nums[high] == target) res.add(Arrays.asList(nums[low++], nums[high--]));
-            else if (nums[low] + nums[high] > target) high--;
+            if (nums[low] + nums[high] == tar) {
+                res.add(Arrays.asList(nums[low], nums[high]));
+                low++;
+                high--;
+            }
+            else if (nums[low] + nums[high] > tar) high--;
             else low++;
         }
 

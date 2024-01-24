632f3a86601aa1d0ba7acbf9b5a302dee391db63
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-01-24 07:17:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (84.26%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int next = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = next;
+        }
+
+        return prev1;
+    }
+}

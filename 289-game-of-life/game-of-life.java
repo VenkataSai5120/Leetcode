16ebe9fb4bd7de57a75075a23d2b86d862f81064
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-02-28 08:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (40.89%) - LeetSync

---
+++ 289-game-of-life/game-of-life.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[] dx = { +1, +1, 0, -1, -1, -1, 0, +1 };
+    int[] dy = { 0, +1, +1, +1, 0, -1, -1, -1 };
+
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        changeState(board, 0, 0, m, n);
+    }
+
+    private void changeState(int[][] board, int i, int j, int m, int n) {
+        if (j == n) {
+            i++;
+            j = 0;
+            if (i == m) {
+                return;
+            }
+        }
+
+        int nextGen = countAlive(board, i, j, m, n);
+        changeState(board, i, j + 1, m, n);
+        if (board[i][j] == 1) {
+            if (nextGen < 2 || nextGen > 3) {
+                board[i][j] = 0;
+            }
+            else if (nextGen == 3 || nextGen == 2){
+                board[i][j] = 1;
+            }
+        }
+        else if (nextGen == 3) {
+            board[i][j] = 1;
+        }
+    }
+
+    private int countAlive(int[][] board, int i, int j, int m, int n) {
+        int alive = 0;
+
+        for (int k = 0; k < 8; k++) {
+            int x = i + dx[k], y = j + dy[k];
+            if (Math.min(x, y) < 0 || x >= m || y >= n) continue;
+            if (board[x][y] == 1) alive++;
+        }
+
+        return alive;
+    }
+}

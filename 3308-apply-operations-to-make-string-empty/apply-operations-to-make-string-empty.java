f0f4983daf4d93434d80c65e3399cfe8dfa149a8
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-02-28 23:04:16 +0530

    Time: 24 ms (75.86%) | Memory: 48.8 MB (82.40%) - LeetSync

--- 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.java
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.java
@@ -1,33 +1,19 @@
 class Solution {
     public String lastNonEmptyString(String s) {
         int[] fre = new int[26];
-        Set<Character> lastChars = new HashSet<>();
         int maxFre = 0;
         StringBuilder lastString = new StringBuilder();
 
         for (char c : s.toCharArray()) {
             fre[c - 'a']++;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (fre[i] == 0) {
-                continue;
-            }
-            else if (fre[i] > maxFre) {
-                maxFre = fre[i];
-                lastChars.clear();
-                lastChars.add((char)(i + 'a'));
-            }
-            else if (fre[i] == maxFre) {
-                lastChars.add((char)(i + 'a'));
-            }
+            maxFre = Math.max(maxFre, fre[c - 'a']);
         }
 
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
-            if (lastChars.contains(c)) {
+            if (fre[c - 'a'] == maxFre) {
                 lastString.append(c);
-                lastChars.remove(c);
+                fre[c - 'a']--;
             }
         }
 

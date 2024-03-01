6d0096e9b26eeaaeb3dc254db7ac0cc5b1db9770
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-02 00:27:09 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (18.32%) - LeetSync

---
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxHeight = 0;
+    int leaveSum = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        findDeepestLeaveSum(root, 1, null);
+        return leaveSum / 2;
+    }
+
+    private void findDeepestLeaveSum(TreeNode root, int height, TreeNode par) {
+        if (root == null) {
+            if (height > maxHeight) {
+                maxHeight = height;
+                leaveSum = par.val;
+            }
+            else if (maxHeight == height) {
+                leaveSum += par.val;
+            }
+            return;
+        }
+
+        findDeepestLeaveSum(root.left, height + 1, root);
+        findDeepestLeaveSum(root.right, height + 1, root);
+    }
+}

d7c968ebd6612566c9e94357dce762dbf53e4306
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-11 23:59:37 +0530

    Time: 122 ms (19.54%) | Memory: 63.3 MB (13.79%) - LeetSync

--- 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.java
+++ 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.java
@@ -17,7 +17,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                if (isPalindrome(s, 0, i) == 1 && isPalindrome(s, i + 1, j) == 1 && isPalindrome(s, j + 1, n - 1) == 1) {
+                if (isPalindrome(s, 0, i) == 1 && 
+                isPalindrome(s, i + 1, j) == 1 && isPalindrome(s, j + 1, n - 1) == 1) {
                     return true;
                 }
             }

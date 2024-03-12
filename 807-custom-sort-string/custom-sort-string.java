066b275c6d8c2fd11a86c95d7b8e2efc00f62ad8
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-12 22:31:06 +0530

    Time: 4 ms (24.25%) | Memory: 41.3 MB (85.88%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public String customSortString(String order, String s) {
         Character[] arr = new Character[s.length()];
+
         for (int i = 0; i < s.length(); i++) {
             arr[i] = s.charAt(i);
         }
 
         Arrays.sort(arr, new Comparator<Character>() {
-            @Override
             public int compare(Character a, Character b) {
                 return order.indexOf(a) - order.indexOf(b);
             }
@@ -18,6 +18,7 @@ class Solution {
         for (char c : arr) {
             res.append(c);
         }
+
         return res.toString();
     }
 }

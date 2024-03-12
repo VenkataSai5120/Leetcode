d7faa0c6d48a4d8eb40213ff285b219a41d4d871
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-13 00:57:22 +0530

    Time: 11 ms (36.71%) | Memory: 41 MB (55.72%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char k = '1'; k <= '9'; k++) {
+                        if (isValid(board, i, j, k)) {
+                            board[i][j] = k;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+                            else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int r, int c, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[r][i] == ch) return false;
+            if (board[i][c] == ch) return false;
+            if (board[3 * (r / 3) + i / 3][3 * (c / 3) + i % 3] == ch) return false;
+        }
+
+        return true;
+    }
+}

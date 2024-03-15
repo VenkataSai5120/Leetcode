292a9df2463e90cf54e49d108547b220179f3992
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-15 21:58:04 +0530

    Time: 19 ms (72.08%) | Memory: 98.1 MB (38.07%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -1,38 +1,41 @@
 class Solution {
-    private static final int[] DIR_X = {1, -1, 0, 0};
-    private static final int[] DIR_Y = {0, 0, 1, -1};
-    
-    private boolean dfs(int curX, int curY, int lastX, int lastY, int n, int m, boolean[][] vis, char[][] grid, char startChar) {
-        vis[curX][curY] = true;
-        
-        for (int i = 0; i < 4; ++i) {
-            int newX = curX + DIR_X[i];
-            int newY = curY + DIR_Y[i];
-            if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                if (!(newX == lastX && newY == lastY)) {
-                    if (grid[newX][newY] == startChar) {
-                        if (vis[newX][newY] || 
-                        dfs(newX, newY, curX, curY, n, m, vis, grid, startChar)) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    
+    private final int[] dirX = {1, -1, 0, 0};
+    private final int[] dirY = {0, 0, 1, -1};
+
     public boolean containsCycle(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                if (!vis[i][j] && dfs(i, j, -1, -1, n, m, vis, grid, grid[i][j])) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] vis = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!vis[i][j] && isCycleDetected(i, j, -1, -1, grid[i][j], grid, vis)) {
                     return true;
                 }
             }
         }
+
+        return false;
+    }
+    private boolean isCycleDetected(int r, int c, int pr, int pc, char currC, 
+    char[][] grid, boolean[][] vis) {
+        vis[r][c] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int newR = r + dirX[k], newC = c + dirY[k];
+            if (Math.min(newR, newC) < 0 || newR >= grid.length || newC >= grid[0].length 
+            || grid[newR][newC] != currC) {
+                continue;
+            }
+
+            if (newR != pr && newC != pc && vis[newR][newC]) {
+                return true;
+            }
+
+            if (!vis[newR][newC] && isCycleDetected(newR, newC, r, c, currC, grid, vis)) {
+                return true;
+            }
+        }
+
         return false;
     }
 }

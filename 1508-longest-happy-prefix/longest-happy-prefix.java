e3243086fcbe4d2258562f5bffc33631dee18e0b
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-16 14:13:37 +0530

    Time: 12 ms (38.61%) | Memory: 45.6 MB (22.04%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] kmp = new int[n + 1];
+        // int i = 1, j = 0;
+
+        for (int i = 1, j = 0; i < s.length();)
+        if (s.charAt(i) == s.charAt(j)) 
+            kmp[++i] = ++j;
+        else {
+            i += (j == 0) ? 1 : 0;
+            j = kmp[j];
+        }
+
+        return s.substring(0, kmp[n]);
+    }
+}

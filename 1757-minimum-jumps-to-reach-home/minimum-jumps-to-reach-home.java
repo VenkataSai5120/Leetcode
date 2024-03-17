725f69710d329caac8103167ccc1882f19a4bea6
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-17 11:15:42 +0530

    Time: 19 ms (89.89%) | Memory: 44.8 MB (78.65%) - LeetSync

---
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int bfs(Set<Integer> vis, int a, int b, int x) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 1});
+
+        int ans = 0;
+        while (!q.isEmpty()) {
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                int[] current = q.poll();
+                int curr = current[0];
+                int flag = current[1];
+
+                if (curr == x) return ans;
+                if (vis.contains(curr)) continue;
+
+                vis.add(curr);
+                if (flag == 1 && (curr - b >= 0)) q.offer(new int[]{curr - b, 0});
+
+                if ((curr - b) <= 2000) q.offer(new int[]{curr + a, 1});
+            }
+            ans++;
+        }
+        return -1;
+    }
+
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+        Set<Integer> vis = new HashSet<>();
+        for (int z : forbidden) vis.add(z);
+
+        return bfs(vis, a, b, x);
+    }
+}

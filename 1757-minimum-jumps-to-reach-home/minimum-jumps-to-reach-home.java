977bd8f251106d4249813f155df70af3a3d9809a
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-17 11:32:17 +0530

    Time: 18 ms (91.01%) | Memory: 44.9 MB (64.61%) - LeetSync

--- 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.java
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.java
@@ -1,34 +1,47 @@
 class Solution {
-    public int bfs(Set<Integer> vis, int a, int b, int x) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 1});
-
-        int ans = 0;
-        while (!q.isEmpty()) {
-            int n = q.size();
-
-            for (int i = 0; i < n; i++) {
-                int[] current = q.poll();
-                int curr = current[0];
-                int flag = current[1];
-
-                if (curr == x) return ans;
-                if (vis.contains(curr)) continue;
-
-                vis.add(curr);
-                if (flag == 1 && (curr - b >= 0)) q.offer(new int[]{curr - b, 0});
-
-                if ((curr + a) <= 10000) q.offer(new int[]{curr + a, 1});
-            }
-            ans++;
-        }
-        return -1;
-    }
-
     public int minimumJumps(int[] forbidden, int a, int b, int x) {
         Set<Integer> vis = new HashSet<>();
-        for (int z : forbidden) vis.add(z);
+        for (int ele : forbidden) {
+            vis.add(ele);
+        }
+        return findMinJumps(a, b, x, vis);
+    }
 
-        return bfs(vis, a, b, x);
+    private int findMinJumps(int a, int b, int x, Set<Integer> vis) {
+        int minJumps = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 1});
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                int[] details = q.poll();
+                int pos = details[0];
+                int flag = details[1];
+
+                if (x == pos) {
+                    return minJumps;
+                }
+
+                if (vis.contains(pos)) {
+                    continue;
+                }
+
+                vis.add(pos);
+
+                if (pos - b >= 0 && flag == 1) {
+                    q.add(new int[] {pos - b, 0});
+                }
+
+                if (pos - b <= 2000) {
+                    q.add(new int[] {pos + a, 1});
+                }
+            }
+
+            minJumps++;
+        }
+
+        return -1;
     }
 }

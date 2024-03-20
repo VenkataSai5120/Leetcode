52afe730583f7af8c8ceb6734881df5e0ed38905
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-03-20 09:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.33%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.java
+++ 233-number-of-digit-one/number-of-digit-one.java
@@ -11,12 +11,12 @@ class Solution {
     private int countOnes(String num, int index, int decider, int count, int n, int[][][] dp) {
         if (index == n) return count;
         if (dp[index][decider][count] != -1) return dp[index][decider][count];
-        int ones = 0;
+        // int ones = 0;
         int limit = decider == 1 ? 9 : num.charAt(index) - '0';
         int res = 0;
 
         for (int i = 0; i <= limit; i++) {
-            if (i == 1) ones++;
+            // if (i == 1) ones++;
             int newCount = i == 1 ? count + 1 : count;
             int newDecider = i < limit ? 1 : decider;
             res += countOnes(num, index + 1, newDecider, newCount, n, dp);

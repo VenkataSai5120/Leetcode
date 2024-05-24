0e27ee467d9bff4e1a13dd955a6a6b1e5605a617
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-05-24 15:20:34 +0530

    Time: 7 ms (94.96%) | Memory: 56.7 MB (57.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
-        find(s, res, new ArrayList<>(), 0);
+        findValidPartitions(s, 0, res, new ArrayList<>());
         return res;
     }
 
-    public void find(String s, List<List<String>> res, List<String> ds, int index) {
+    private void findValidPartitions(String s, int index, List<List<String>> res, List<String> dup) {
         if (index == s.length()) {
-            res.add(new ArrayList<>(ds));
+            res.add(new ArrayList<>(dup));
             return;
         }
 
         for (int i = index; i < s.length(); i++) {
             if (isPalindrome(s, index, i)) {
-                ds.add(s.substring(index, i + 1));
-                find(s, res, ds, i + 1);
-                ds.remove(ds.size() - 1);
+                dup.add(s.substring(index, i + 1));
+                findValidPartitions(s, i + 1, res, dup);
+                dup.remove(dup.size() - 1);
             }
         }
     }
 
-    public boolean isPalindrome(String s, int low, int high) {
+    private boolean isPalindrome(String s, int low, int high) {
         while (low < high) {
-            if (s.charAt(low++) != s.charAt(high--)) return false;
+            if (s.charAt(low) != s.charAt(high)) return false;
+            low++; high--;
         }
-
         return true;
     }
 }

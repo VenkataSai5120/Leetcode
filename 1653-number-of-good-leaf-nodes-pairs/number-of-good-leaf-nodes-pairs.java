4571fc3bc8000f20df9ea813361dba65ff8e40e3
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2024-07-18 23:03:24 +0530

    Time: 2 ms (95.76%) | Memory: 44.7 MB (55.42%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -34,7 +34,7 @@ class Solution {
         int[] leftDistances = dfs(node.left, distance);
         int[] rightDistances = dfs(node.right, distance);
 
-        for (int l = 1; l <= distance; l++) {
+        for (int l = 0; l <= distance; l++) {
             for (int r = distance; r >= 0; r--) {
                 if (l + r <= distance) {
                     result += leftDistances[l] * rightDistances[r];

f602c34ffbacaf99d83e10f57ad6c571f23656a4
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2025-08-31 22:48:58 +0530

    Time: 2 ms (11.47%) | Memory: 43 MB (95.75%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,14 +14,20 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-
-    private boolean isValid(TreeNode root, long minValue, long maxValue) {
+	public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
-        if (root.val >= maxValue || root.val <= minValue) return false;
-        return isValid(root.left, minValue, root.val) && 
-        isValid(root.right, root.val, maxValue);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
 }

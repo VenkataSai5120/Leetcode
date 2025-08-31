ca50a3c4aed9f4ec715d0f1b2d7bd70c8e020861
BALAGANI VENKATA SAI <vsai5120@gmail.com>
2025-08-31 18:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (41.07%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i=0;i<matrix.length;i++){
+            for (int j=i + 1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for (int i=0;i<matrix.length;i++)
+            matrix[i]=Reverse(matrix[i]);
+        
+        // System.out.println(java.util.Arrays.deepToString(matrix));
+    }
+    public static int[] Reverse(int[] matrix){
+        for (int i=0;i<matrix.length/2;i++){
+            int temp=matrix[i];
+            matrix[i]=matrix[matrix.length-1-i];
+            matrix[matrix.length-1-i]=temp;
+        }
+        return matrix;
+    }
+}
